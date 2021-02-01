ee932e060f8e6078aa38f016832bff834af3e36f
Kurt Opel <kurt.kurtopel.com>
2021-02-01 18:01:00 -0500

    Converted to postgresql

--- web-api/db/schema.rb
+++ web-api/db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2021_01_28_185515) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.boolean "done"
-    t.integer "todo_id", null: false
+    t.bigint "todo_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["todo_id"], name: "index_items_on_todo_id"
