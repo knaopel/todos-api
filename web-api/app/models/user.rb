7da8c796fa2d22c2d8ebc934ed5a50fc56c2d6be
Kurt Opel <kurt.kurtopel.com>
2021-02-18 18:47:16 -0500

    todos werher missing

--- web-api/app/models/user.rb
+++ web-api/app/models/user.rb
@@ -9,19 +9,11 @@ class User < ApplicationRecord
   end
 
   # Model associations
-  # has_many :todos, foreign_key: :created_by
-  # has_many :user_honey_assignments, :class_name => "Honey", :foreign_key => "user_id"
-  # has_many :honeys, :through => :user_honey_assignments
-  # has_many :honey_user_assignments, :class_name => "Honey", :foreign_key => "honey_id"
-  # has_many :users, :through => :honey_user_assignments
+  has_many :todos, foreign_key: :created_by
+
   # Validations
   validates_presence_of :name, :email, :password_digest
   validates :username, presence: true,
     format: { with: /\A(?=.*[a-z])[a-z\d]+\Z/i },
     uniqueness: { case_sensitive: false }
 end
-
-# class Honey < ApplicationRecord
-#   belongs_to :user, :class_name => "User"
-#   belongs_to :honey, :class_name => "User"
-# end
