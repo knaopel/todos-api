557a3d9762bf8e12ca815a878492f64b12d39bfb
Kurt Opel <kurt@kurtopel.com>
2021-05-19 13:59:02 -0400

    Follow and Honey Tests

--- web-api/spec/models/follow_spec.rb
+++ web-api/spec/models/follow_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Follow, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association Test
+  it { should belong_to(:follower) }
+  it { should belong_to(:following) }
 end
--- web-api/spec/models/honey_spec.rb
+++ web-api/spec/models/honey_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Honey, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association Test
+  it { should belong_to(:honey) }
+  it { should belong_to(:dewer) }
 end
