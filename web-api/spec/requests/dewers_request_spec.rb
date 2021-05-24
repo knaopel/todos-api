5c7e384c79be56af4c62ac296e87fdc6e3ac4cc4
Kurt Opel <kurt@kurtopel.com>
2021-05-24 17:38:29 -0400

    cleaned up tests

--- web-api/spec/requests/dewers_request_spec.rb
+++ web-api/spec/requests/dewers_request_spec.rb
@@ -2,66 +2,38 @@ require 'rails_helper'
 
 RSpec.describe "Dewers", type: :request do
   let!(:user) { create(:user) }
-  let(:headers) { valid_headers.except('Authorization') }
   let!(:user2) { create(:user) }
   let!(:user3) { create(:user) }
-  let(:valid_credentials) do
-    {
-      email: user.email,
-      password: user.password
-    }.to_json
-  end
+  let(:headers) { valid_headers }
   # Dewers request suite
   describe 'GET /dewers' do
     context 'when there is one dewer' do
       before do
-        #log in first user
-        post '/auth/login', params: valid_credentials, headers: headers
-        headers["Authorization"] = json["auth_token"]
-        honey_params = { email: user2.email }.to_json
-        # add user 2 as honey
-        post '/honeys', params: honey_params, headers: headers
-        # log in as second user
-        user2_credentials = { email: user2.email, password: user2.password }.to_json
-        post '/auth/login', params: user2_credentials, headers: headers
-        headers["Authorization"] = json["auth_token"]
+        user2.add_honey(user.id)
         get '/dewers', headers: headers
       end
       it 'returns success status' do
         expect(response).to have_http_status(:ok)
       end
-      it 'returns an array with user one' do
+      it 'returns an array with user2' do
         expect(json.count).to eq(1)
-        expect(json[0]["email"]).to eq(user.email)
+        expect(json[0]["email"]).to eq(user2.email)
       end
     end
 
-    context 'when there is two dewers' do
+    context 'when there are two dewers' do
       before do
-         #log in first user
-         post '/auth/login', params: valid_credentials, headers: headers
-         headers["Authorization"] = json["auth_token"]
-         honey_params = { email: user2.email }.to_json
-         # add user2 as honey to user1
-         post '/honeys', params: honey_params, headers: headers
-         # log in user3
-         user3_credentials = { email: user3.email, password: user3.password }.to_json
-         post '/auth/login', params: user3_credentials, headers: headers.except('Authorization')
-         # add user2 as honey to user3
-         headers["Authorization"] = json["auth_token"]
-         post '/honeys', params: honey_params, headers: headers
-         # login as user2
-         user2_credentials = { email: user2.email, password: user2.password }.to_json
-         post '/auth/login', params: user2_credentials, headers: headers
-         headers["Authorization"] = json["auth_token"]
-         get '/dewers', headers: headers
+        # add user as honey for user2 and user3
+        user2.add_honey(user.id)
+        user3.add_honey(user.id)
+        get '/dewers', headers: headers
       end
       it 'returns a success status' do
         expect(response).to have_http_status(:ok)
       end
-      it 'returns an array of the users 1 and 3' do
+      it 'returns an array of the users 2 and 3' do
         expect(json.count).to eq(2)
-        expect(json[0]["email"]).to eq(user.email)
+        expect(json[0]["email"]).to eq(user2.email)
         expect(json[1]["email"]).to eq(user3.email)
       end
     end
--- web-api/spec/requests/honeys_request_spec.rb
+++ web-api/spec/requests/honeys_request_spec.rb
@@ -1,26 +1,35 @@
 require 'rails_helper'
 
-RSpec.describe "Honeys", type: :request do
+RSpec.describe "Honeys API", type: :request do
   let!(:user) { create(:user) }
   let(:honeys_count) { user.honeys_count }
-  let(:headers) { valid_headers.except('Authorization') }
+  let(:headers) { valid_headers }
   let!(:user2) { create(:user) }
-  let(:valid_credentials) do
-    {
-      email: user.email,
-      password: user.password
-    }.to_json
+  let!(:honey_users) { create_list(:user, 5) }
+
+  # Honey GET test Suite
+  describe 'GET /honeys' do
+    before do
+      honey_users.each do |u|
+        user.add_honey(u.id)
+      end
+      # debugger
+      get '/honeys', headers: headers
+    end
+    it 'returns honeys' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(5)      
+    end
+    it 'returns "OK" status code' do
+      expect(response).to have_http_status(:ok)
+    end
   end
 
   # Honey add test suite
   describe 'POST /honeys' do
     context 'when user is found' do
       before do
-        # log in first user
-        post '/auth/login', params: valid_credentials, headers: headers
-        headers["Authorization"] = json["auth_token"]
         honey_params = { email: user2.email }.to_json
-        # add second user as honey
         post '/honeys', params: honey_params, headers: headers
       end
       it 'returns success status' do
@@ -33,9 +42,6 @@ RSpec.describe "Honeys", type: :request do
     end
     context 'when user is not found' do
       before do
-        # log in user
-        post '/auth/login', params: valid_credentials, headers: headers
-        headers["Authorization"] = json["auth_token"]
         honey_params = { email: Faker::Internet.email }.to_json
         # attempt to add fake user as honey
         post '/honeys', params: honey_params, headers: headers        
@@ -44,12 +50,8 @@ RSpec.describe "Honeys", type: :request do
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
-
     context 'when user is same user' do
       before do
-        # log in user
-        post '/auth/login', params: valid_credentials, headers: headers
-        headers["Authorization"] = json["auth_token"]
         honey_params = { email: user.email }.to_json
         # attempt to add same user as honey
         post '/honeys', params: honey_params, headers: headers        
@@ -58,5 +60,12 @@ RSpec.describe "Honeys", type: :request do
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
+  end
+
+  describe 'POST /honeys/exists' do
+    
+  end
+  describe 'DELETE /honeys/:id' do
+    
   end
 end
