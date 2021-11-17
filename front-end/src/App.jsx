c901b1c788f01841cc5cec00afdb5d5996c05c5c
Kurt Opel <kurt.opel@sogeti.com>
2021-11-16 21:53:06 -0500

    front-end wip

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,39 +1,45 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from './pages/Login';
-import './App.css';
-import Signup from './pages/Signup';
-import Home from './pages/Home';
-import { createMuiTheme, ThemeProvider as MuiThemeProvider } from '@material-ui/core';
-import PartnerLogins from './pages/PartnerLogins';
-import PartnerLogin from './pages/PartnerLogin';
+import "./App.css";
+// import Signup from './pages/Signup';
+import Home from "./pages/Home";
+// import { createMuiTheme, ThemeProvider as MuiThemeProvider } from '@material-ui/core';
+// import PartnerLogins from './pages/PartnerLogins';
+// import PartnerLogin from './pages/PartnerLogin';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#33c9dc',
-      main: '#FF5722',
-      dark: '#d50000',
-      contrastText: '#fff'
-    }
-  }
-});
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-function App() {
+// const theme = createMuiTheme({
+//   palette: {
+//     primary: {
+//       light: '#33c9dc',
+//       main: '#FF5722',
+//       dark: '#d50000',
+//       contrastText: '#fff'
+//     }
+//   }
+// });
+
+const App = () => {
   return (
-    <MuiThemeProvider theme={theme}>
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/" component={Home} />
-            <Route path="/partnerlogin/:provider" component={PartnerLogin} />
-            <Route path="/partnerlogin" component={PartnerLogins} />
-          </Switch>
-        </div>
-      </Router>
-    </MuiThemeProvider>
+    // <MuiThemeProvider theme={theme}>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/login" element={<Login history={location.history} />} />
+          {/* <Route exact path="/signup" component={Signup} /> */}
+          <Route exact path="/" element={<Home />} />
+          {/* <Route path="/partnerlogin/:provider" component={PartnerLogin} /> */}
+          {/* <Route path="/partnerlogin" component={PartnerLogins} /> */}
+        </Routes>
+        {/* <Button variant="contained">Hello There!</Button> */}
+      </div>
+    </Router>
+    // </MuiThemeProvider>
   );
-}
+};
 
 export default App;
