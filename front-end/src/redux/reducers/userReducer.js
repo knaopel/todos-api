540a47a2969a22294e06d8d0dfb8653f8a607a73
Kurt Opel <kurt.opel@sogeti.com>
2021-12-08 08:25:12 -0500

    merge authReducer into userReducer

--- front-end/src/redux/reducers/userReducer.js
+++ front-end/src/redux/reducers/userReducer.js
@@ -3,8 +3,12 @@ import initialState from './initialState';
 
 export default function userReducer(state = initialState.user, action) {
   switch (action.type) {
+    case types.USER_LOGIN_SUCESS:
+    case types.USER_GET_LOCAL_COMPLETE:
+      return action.user;
     case types.USER_GET_SUCCESS:
       return { ...state, ...action.user };
+    case types.USER_SET_LOCAL:
     default:
       return state;
   }
